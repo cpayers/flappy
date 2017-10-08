789d3e62ed67c56001c4c721ff1b67b9b22390eb
cpayers <cpayers1@gmail.com>
2017-10-08 16:03:28 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,67 @@
+var GRAVITY = 0.3;
+var SPEED = 3;
 var bird;
+var score;
 var pipes = [];
 
+
 function setup() {
-    createCanvas(400, 600);
-    bird = new Bird();
-    pipes.push(new Pipe());
+    createCanvas(600, 600);
+    bird = new Bird(50, height / 2, 15);
+    score = 0;
+    textAlign(CENTER);
+    textSize(40);
 }
 
 function draw(){
+    
     background(0, 155, 255);
     
-    for (var i = pipes.length-1; i >= 0; i--) {
-        pipes[i].show();
-        pipes[i].update();
-        
-        if (pipes[i].hits(bird)) {
-            console.log("HIT");
-        }
-        
-        if (pipes[i].offscreen()) {
-            pipes.splice(i, 1);
-        }
-    }
-    
     bird.update();
-    bird.show();
+    bird.draw();
     
-    if (frameCount % 75 == 0) {
-        pipes.push(new Pipe());
+    if(frameCount % 60 === 0) {
+        pipes.push(new Pipe(width, random(height / 2) + height /4, 200, color(score % 255, random(255), random(255))));
     }
+    
+    handlePipes();
+    
+    noStroke();
+    text(score, 50, height - 20);
+}
+
+function handlePipes(){
+      
+    
+    for(var i = 0; i < pipes.length; i++) {
+    pipes[i].update();
+    pipes[i].draw();
+        
+        if (bird.collidesWith(pipes[i])) { // if the player hits a pipe
+
+    noLoop();
+	noStroke();
+	textSize(50);
+	text("You lose!", width / 2, height / 2);
+	textSize(40);
+	text("Press f5 to restart!", width / 2, height / 2 + 30);
+		} else if (!pipes[i].passed) { // if we haven't passed the pipe yet
+
+			if (pipes[i].isPassed(bird.x)) // check to see if we pass it
+				score++;
+    }
+    
+ }
     
 }
 
+
 function keyPressed() {
     if (key == ' ') {
-        bird.up();
+        bird.up(-6);
         console.log("SPACE");
     }
+	
 }
+
+
